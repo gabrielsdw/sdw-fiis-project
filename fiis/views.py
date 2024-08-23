c5106fd2c145562314e916ed27113eff621a83e6
gabrielsdw <gabriel2006adriano74@gmail.com>
2024-08-23 10:54:36 -0300

    third commit

--- fiis/views.py
+++ fiis/views.py
@@ -12,14 +12,19 @@ class FiisListView(generic.ListView):
     def get_queryset(self):
         queryset = super().get_queryset()
         request = self.request.GET
-        print(request)
-        search = request.get('search', '')
+        print(request)  
+        search = request.get('search', '') 
+        segment = request.get('segment', '')
         if search:
             queryset = queryset.filter(name__icontains=search)
+        if segment:
+            queryset = queryset.filter(indicators__segmento__icontains=segment)
+        
         return queryset
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['selected_segment'] = self.request.GET.get('segment', '')
         context['segments'] = models.Fii.objects.values_list('indicators__segmento', flat=True).distinct()
         return context
 
